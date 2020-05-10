23824cc3b9fc774863635f765d183546e8c5c928
russel5 <rnuriyev82@gmail.com>
2020-05-10 12:36:42 +0500

    fix camera error after update

--- Android.mk
+++ Android.mk
@@ -17,5 +17,12 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),lilac)
-
+include $(CLEAR_VARS)
+LOCAL_MODULE := SemcCameraUI-xhdpi-release
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := proprietary/priv-app/$(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
 endif
